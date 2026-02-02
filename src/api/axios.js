51e4cca457d0d2f03d7b4935083293505ca43323
kuldeep-malviya <kuldeepmalviya0277@gmail.com>
2026-02-02 17:52:18 +0530

    push

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,15 +1,16 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: import.meta.env.VITE_API_URL
+  baseURL: import.meta.env.VITE_API_URL,
 });
 
-// token auto attach
 API.interceptors.request.use((req) => {
   const token = localStorage.getItem("token");
+
   if (token) {
-    req.headers.Authorization = token;
+    req.headers.Authorization = `Bearer ${token}`;
   }
+  
   return req;
 });
 
