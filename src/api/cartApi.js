77630b2ef48c424d97513a9db6229b32b1f3cffa
kuldeep-malviya <kuldeepmalviya0277@gmail.com>
2026-02-03 15:42:45 +0530

    Refactor authHeader to use getAuthHeader function

--- src/api/cartApi.js
+++ src/api/cartApi.js
@@ -1,51 +1,54 @@
 import axios from "axios";
-import { toast } from "react-toastify";
 
-const token = localStorage.getItem("token");
+const getAuthHeader = () => {
+  const token = localStorage.getItem("token");
 
-const authHeader = {
-  headers: {
-    Authorization: `Bearer ${token}`,
-  },
+  return {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  };
 };
 
 // Add item to cart
 export const addToCart = async (productId) => {
- const res =   await axios.post(`${import.meta.env.VITE_API_URL}/cart/add`, {
-    productId,
-    quantity: 1,
-    
-  },
-authHeader);
-if(res.status === 200){
-  toast.success("item successfully add in cart")
-}
-return res
+  return await axios.post(
+    `${import.meta.env.VITE_API_URL}/cart/add`,
+    {
+      productId,
+      quantity: 1,
+    },
+    getAuthHeader()
+  );
 };
 
-// Existing functions
+// Get cart
 export const getCart = async () => {
-  return await axios.get(`${import.meta.env.VITE_API_URL}/cart`,
-    authHeader,
+  return await axios.get(
+    `${import.meta.env.VITE_API_URL}/cart`,
+    getAuthHeader()
   );
 };
 
 export const increaseQty = async (cartId) => {
-  return await axios.put(`${import.meta.env.VITE_API_URL}/cart/increase/${cartId}`,
+  return await axios.put(
+    `${import.meta.env.VITE_API_URL}/cart/increase/${cartId}`,
     {},
-    authHeader,
+    getAuthHeader()
   );
 };
 
 export const decreaseQty = async (cartId) => {
-  return await axios.put(`${import.meta.env.VITE_API_URL}/cart/decrease/${cartId}`,
-     {},
-    authHeader,
+  return await axios.put(
+    `${import.meta.env.VITE_API_URL}/cart/decrease/${cartId}`,
+    {},
+    getAuthHeader()
   );
 };
 
 export const removeItem = async (cartId) => {
-  return await axios.delete(`${import.meta.env.VITE_API_URL}/cart/${cartId}`,
-    authHeader
+  return await axios.delete(
+    `${import.meta.env.VITE_API_URL}/cart/${cartId}`,
+    getAuthHeader()
   );
 };
